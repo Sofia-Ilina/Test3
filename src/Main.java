509a55cc2d897512094c2bef9f88a91cfec384e8
Sofiya <925038623sss@gmail.com>
2024-06-04 23:42:20 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
+        int amountOfMoney = 800; // начальный счет
+        int replenishmentAmount = 200; // сумма пополнения
+        int oneRuble = 100; // 1 рубль = 100 бонусам
+
+        int bonus = 0;
+        if (replenishmentAmount  > 1000) {
+            bonus = replenishmentAmount  / 1000;
+        }
+        int finalBalance = amountOfMoney + replenishmentAmount + bonus;
+        System.out.println("Итог:" + amountOfMoney);
+        System.out.println(bonus);
+
+        // в итоге на экрпне получилось 800
+    }
 }
